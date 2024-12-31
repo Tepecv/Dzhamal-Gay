49fb90724e3fb9f373cc2fc4df972aab7633460a
TepecV <arnur4ik0824@mail.ru>
2024-12-31 13:53:13 +0300

    gay

---
+++ kak/calculate.go
@@ -0,0 +1,6 @@
+package kak
+
+func Calculate(num1, num2 string) string {
+	result := num1 + " " + num2
+	return result
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"kak"
+)
+
+func main() {
+	vivod := kak.Calculate("Dzhamal", "gay")
+	fmt.Println(vivod)
+}
